597c7ec95ad0372d184d556ec333df9d2123c7f6
Raymond Ottun <147501409+raysca@users.noreply.github.com>
2026-01-28 09:13:54 +0000

    docs: migration plan

--- apps/api-next/src/index.ts
+++ apps/api-next/src/index.ts
@@ -2,6 +2,7 @@ import { serve } from "bun";
 import index from "./index.html";
 
 const server = serve({
+  port: 8080,
   routes: {
     // Serve index.html for all unmatched routes.
     "/*": index,
